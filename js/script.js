cfc57ee1d44ffa69e2d62fb38ab42831c484616f
Ivan <pipkamax1993@gmail.com>
2020-06-22 20:55:01 +0300

    finish

--- js/script.js
+++ js/script.js
@@ -15,56 +15,93 @@ var feedbackPopup = document.querySelector('.feedback-popup');
 var feedBackButton = document.querySelector('.feed-back-button');
 var mapWidget = document.querySelector('.map-widget');
 var mapContacts = document.querySelector('.map-contacts');
-var mapClose = document.querySelector('.map-close'); 
+var mapClose = document.querySelector('.map-close');
+var buttonBuy = document.querySelectorAll('.button-buy');
+var storeBasket = document.querySelector('.store-basket');
+var orderClose = document.querySelector('.order-close');
+var form = document.querySelector(".feedback-popup");
+var nameInput = form.querySelector("[name=name]"); 
+var emailInput = form.querySelector("[name=e-mail]"); 
+var textArea = form.querySelector("[name=content-letter]");
 
+form.addEventListener('submit', function (evt) {
+  evt.preventDefault();
+  if (!nameInput.value || !emailInput.value || !textArea.value) {
+    feedbackPopup.classList.remove('popup-show');
+    feedbackPopup.classList.remove('popup-error');
+    form.offsetWidth = form.offsetWidth;
+    feedbackPopup.classList.add('popup-error');
+  }
+});
+
+function myFunction() {
+  for (let i = 0; i < buttonBuy.length; i++) {
+    buttonBuy[i].addEventListener('click', function (evt) {
+      evt.preventDefault();
+      storeBasket.classList.remove('hide');
+      storeBasket.classList.add('popup-show');
+    })
+  }
+}; 
+
+myFunction();
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    feedbackPopup.classList.remove('popup-error');
+    mapWidget.classList.add('hide');
+    storeBasket.classList.add('hide');
+    feedbackPopup.classList.add('hide')
+  }
+})
 
 sliderButtomLeft[0].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.add('show');
   sliderSectionsRotar.classList.add('hide');
-})
+});
 
 sliderButtomLeft[1].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.add('show');
   sliderSectionsRotar.classList.add('hide');
-})
+});
 
 sliderButtomRight[0].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.remove('show');
   sliderSectionsRotar.classList.remove('hide');
-})
+});
 
 sliderButtomRight[1].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.remove('show');
   sliderSectionsRotar.classList.remove('hide');
-})
+});
 
 sliderRadioFrst[0].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.add('show');
   sliderSectionsRotar.classList.add('hide');
-})
+});
 
 sliderRadioFrst[1].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.add('show');
   sliderSectionsRotar.classList.add('hide');
-})
+});
 
 sliderRadioSec[0].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.remove('show');
   sliderSectionsRotar.classList.remove('hide');
-})
+});
 
 sliderRadioSec[1].addEventListener('click', function (evt) {
   evt.preventDefault();
   sliderSectionsDrill.classList.remove('show');
   sliderSectionsRotar.classList.remove('hide');
-})
+});
 
 services[0].addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -72,7 +109,7 @@ services[0].addEventListener('click', function (evt) {
   servicesDelivery.classList.remove("hide");
   servicesGuarant.classList.add("hide");
   servicesCredit.classList.add("hide");
-})
+});
 
 services[1].addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -81,7 +118,7 @@ services[1].addEventListener('click', function (evt) {
   servicesGuarant.classList.remove("hide");
   servicesGuarant.classList.add("show");
   servicesCredit.classList.add("hide");
-})
+});
 
 services[2].addEventListener('click', function (evt) {
   evt.preventDefault();
@@ -90,60 +127,56 @@ services[2].addEventListener('click', function (evt) {
   servicesGuarant.classList.remove("show");
   servicesCredit.classList.remove("hide");
   servicesCredit.classList.add("show");
-})
+});
 
 servicesList[0].addEventListener('click', function (evt) {
   evt.preventDefault();
   servicesList[0].classList.add("services-list-active");
   servicesList[1].classList.remove("services-list-active");
   servicesList[2].classList.remove("services-list-active");
-})
+});
 
 servicesList[1].addEventListener('click', function (evt) {
   evt.preventDefault();
   servicesList[0].classList.remove("services-list-active");
   servicesList[1].classList.add("services-list-active");
   servicesList[2].classList.remove("services-list-active");
-})
+});
 
 servicesList[2].addEventListener('click', function (evt) {
   evt.preventDefault();
   servicesList[0].classList.remove("services-list-active");
   servicesList[1].classList.remove("services-list-active");
   servicesList[2].classList.add("services-list-active");
-})
+});
 
 feedBackClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.add('hide')
-})
+  feedbackPopup.classList.remove('popup-error');
+});
 
 feedBackButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   feedbackPopup.classList.remove('hide');
-})
+  feedbackPopup.classList.add('popup-show');
+  nameInput.focus();
+});
 
 mapContacts.addEventListener('click', function (evt) {
   evt.preventDefault();
   mapWidget.classList.remove('hide');
-})
+  mapWidget.classList.add('popup-show');
+});
 
 mapClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   mapWidget.classList.add('hide');
-})
-
-var buttonBuy = document.querySelector('.button-buy');
-var storeBasket = document.querySelector('.store-basket');
-var orderClose = document.querySelector('.order-close');
-
-
-buttonBuy.addEventListener('click', function (evt) {
-  evt.preventDefault();
-  storeBasket.classList.remove('hide');
-})
+  mapWidget.classList.remove('popup-show');
+});
 
 orderClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   storeBasket.classList.add('hide');
-})
+  storeBasket.classList.remove('popup-show');
+});
