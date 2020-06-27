df92194e99235f8c3861a0eb2a6306650c2abd8e
Ivan <pipkamax1993@gmail.com>
2020-06-27 21:31:29 +0300

    finish

--- js/script-catalog.js
+++ js/script-catalog.js
@@ -1,26 +1,26 @@
-var buttonBuy = document.querySelectorAll('.button-buy');
-var storeBasket = document.querySelector('.store-basket');
-var orderClose = document.querySelector('.order-close');
+var buttonBuy = document.querySelectorAll(".button-buy");
+var storeBasket = document.querySelector(".store-basket");
+var orderClose = document.querySelector(".order-close");
 
 function myFunction() {
 	for (let i = 0; i < buttonBuy.length; i++) {
-		buttonBuy[i].addEventListener('click', function (evt) {
+		buttonBuy[i].addEventListener("click", function (evt) {
 			evt.preventDefault();
-			storeBasket.classList.remove('hide');
-			storeBasket.classList.add('popup-show');
+			storeBasket.classList.remove("hide");
+			storeBasket.classList.add("popup-show");
 		})
 	}
 }
 
 myFunction();
 
-orderClose.addEventListener('click', function (evt) {
+orderClose.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	storeBasket.classList.add('hide');
+	storeBasket.classList.add("hide");
 });
 
 window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
-		storeBasket.classList.add('hide');
+		storeBasket.classList.add("hide");
 	}
 })
